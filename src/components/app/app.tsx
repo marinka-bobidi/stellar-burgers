68f0a918ec303e6b24bfe0d3b438064e8ec81b13
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-10-13 17:49:44 +0300

    commit

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,7 @@
 import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { AppHeader } from '@components';
 
