04e12ea98611620fcaf8e92c35e3ba55ab13d042
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-12-28 18:52:31 +0300

    serial number

--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -1,19 +1,32 @@
 import { forwardRef, useMemo } from 'react';
 import { TIngredientsCategoryProps } from './type';
-import { TIngredient } from '@utils-types';
+import { TConstructorIngredient, TIngredient } from '@utils-types';
 import { IngredientsCategoryUI } from '../ui/ingredients-category';
-
+import { useSelector } from '../../services/store';
 export const IngredientsCategory = forwardRef<
   HTMLUListElement,
   TIngredientsCategoryProps
 >(({ title, titleRef, ingredients }, ref) => {
   /** DONE: взять переменную из стора */
-  const burgerConstructor = {
+  const buffer = useSelector((state) => state.construct);
+  interface IBurgerConstructor {
+    bun: {
+      _id: string;
+    };
+    ingredients: TConstructorIngredient[];
+  }
+  let burgerConstructor: IBurgerConstructor = {
     bun: {
       _id: ''
     },
     ingredients: []
   };
+  if (buffer.bun !== null) {
+    burgerConstructor.bun = { _id: buffer.bun._id };
+  }
+  if (buffer.ingredients) {
+    burgerConstructor.ingredients = buffer.ingredients;
+  }
 
   const ingredientsCounters = useMemo(() => {
     const { bun, ingredients } = burgerConstructor;
